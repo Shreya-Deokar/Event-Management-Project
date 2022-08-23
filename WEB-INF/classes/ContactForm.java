840b633ba3251b999f80d2b040031762b6020fa5
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:12:38 +0530

    Update ContactForm.java

--- WEB-INF/classes/ContactForm.java
+++ WEB-INF/classes/ContactForm.java
@@ -43,7 +43,7 @@ public class ContactForm extends HttpServlet
 					String message = request.getParameter("message");
 
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					ResultSet rs1 = null;
 					ResultSet rs2 = null;

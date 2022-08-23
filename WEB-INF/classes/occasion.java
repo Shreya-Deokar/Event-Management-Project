b6a87e07ca64dcf3b0d132b08f93c62ba01f4647
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:28:33 +0530

    Update occasion.java

--- WEB-INF/classes/occasion.java
+++ WEB-INF/classes/occasion.java
@@ -43,7 +43,7 @@ public class occasion extends HttpServlet
 
 					String occasion_type = request.getParameter("occasion");
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					ResultSet rs1 = null;
 					ResultSet rs2 = null;

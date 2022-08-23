60717d2171c8551ecadad892e320429f05cabe00
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:16:27 +0530

    Update caterDeleteServlet.java

--- WEB-INF/classes/caterDeleteServlet.java
+++ WEB-INF/classes/caterDeleteServlet.java
@@ -44,7 +44,7 @@ public class caterDeleteServlet extends HttpServlet
 
 					String query = "delete from Item where iname = '"+name+"' and iid = '"+id+"'";
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					stmt.executeUpdate(query);
 				

1c4999b7a3a1186ca187dd943a26c5eeb5d466d1
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:26:47 +0530

    Update eventDeleteServlet.java

--- WEB-INF/classes/eventDeleteServlet.java
+++ WEB-INF/classes/eventDeleteServlet.java
@@ -43,7 +43,7 @@ public class eventDeleteServlet extends HttpServlet
 
 					String query = "delete from event where eid = '"+id+"'";
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					stmt.executeUpdate(query);
 				

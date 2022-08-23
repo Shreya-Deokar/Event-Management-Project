2ce6d207e35e83aeaced6a00708d17cc5c3b5c30
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:08:17 +0530

    Update AddDecoration.java

--- WEB-INF/classes/AddDecoration.java
+++ WEB-INF/classes/AddDecoration.java
@@ -79,7 +79,7 @@ public class AddDecoration extends HttpServlet
 					}
 					
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					ResultSet rs1 = null;
 					ResultSet rs2 = null;

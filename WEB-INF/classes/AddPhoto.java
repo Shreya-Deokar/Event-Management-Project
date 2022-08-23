93eeae68e201b9a634e0032fc3f2e16b723fe219
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:10:07 +0530

    Update AddPhoto.java

--- WEB-INF/classes/AddPhoto.java
+++ WEB-INF/classes/AddPhoto.java
@@ -89,7 +89,7 @@ public class AddPhoto extends HttpServlet
 					}
 					
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					ResultSet rs1 = null;
 					ResultSet rs2 = null;

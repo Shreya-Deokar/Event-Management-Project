88978626b291fdac5cbd79d366975e7b4c3d7344
Shreya-Deokar <91956301+Shreya-Deokar@users.noreply.github.com>
2022-08-23 19:33:55 +0530

    Update selectMakeup1.java

--- WEB-INF/classes/selectMakeup1.java
+++ WEB-INF/classes/selectMakeup1.java
@@ -43,7 +43,7 @@ public class selectMakeup1 extends HttpServlet
 					String no_per = request.getParameter("no_per");
 
 					Class.forName("org.postgresql.Driver");
-					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eventmanage","postgres","mcasc05");
+					Connection con = DriverManager.getConnection("database path","databse","password");
 					Statement stmt = con.createStatement();
 					ResultSet rs1 = null;
 					ResultSet rs2 = null;
